afc31e85710f35ef1145c257de23cd698b804ae5
Dhananjay Sable <dhananjaysable370@gmail.com>
2024-09-07 14:10:23 +0530

    sum of n odd numbers

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
-public class Main{
-    public static void main(String args[]){
-        System.out.println("Hello world");
+public class Main {
+    public static void main(String args[]) {
+        int n = 10;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+                sum += i;
+            }
+        }
+        System.out.println(sum);
     }
 }
