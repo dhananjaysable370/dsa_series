559ca7b24e2a5286e8bcb9d2927afe4ac2b57d31
Dhananjay Sable <dhananjaysable370@gmail.com>
2024-09-09 07:57:54 +0530

    sum of all n factorials

--- Sum.py
+++ Sum.py
@@ -1,12 +1,12 @@
-sumOfOdd = 0
-sumOfEven = 0
+# sumOfOdd = 0
+# sumOfEven = 0
 
-for i in range(1,21):
-    if(i % 2 == 0):
-        sumOfEven += i
-    else:
-        sumOfOdd += i
+# for i in range(1,21):
+#     if(i % 2 == 0):
+#         sumOfEven += i
+#     else:
+#         sumOfOdd += i
 
         
-print(sumOfOdd)
-print(sumOfEven)
+# print(sumOfOdd)
+# print(sumOfEven)
