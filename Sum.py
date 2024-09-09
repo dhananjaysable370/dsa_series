452babdb13b85276c1ab5ac3777304e8bf62e289
Dhananjay Sable <dhananjaysable370@gmail.com>
2024-09-09 08:06:36 +0530

    sum of all n factorials function

--- Sum.py
+++ Sum.py
@@ -1,12 +1,24 @@
-# sumOfOdd = 0
-# sumOfEven = 0
+sumOfOdd = 0
+sumOfEven = 0
 
-# for i in range(1,21):
-#     if(i % 2 == 0):
-#         sumOfEven += i
-#     else:
-#         sumOfOdd += i
+for i in range(1,21):
+    if(i % 2 == 0):
+        sumOfEven += i
+    else:
+        sumOfOdd += i
 
         
-# print(sumOfOdd)
-# print(sumOfEven)
+print(sumOfOdd)
+print(sumOfEven)
+
+def sumOfFactorials(n):
+    fact = 1
+    sum = 0
+    for i in range(1,n+1):
+        fact = fact * i
+        sum = sum + fact
+
+    return sum
+        
+        
+print(sumOfFactorials(10))
