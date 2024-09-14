7e11f620edc281f4be75ac5eac871110e232ec59
Dhananjay Sable <dhananjaysable370@gmail.com>
2024-09-14 22:52:51 +0530

    dsa practice

--- Vectors.cpp
+++ Vectors.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void reverseArray(int arr[])
+{
+    int start = 0;
+    int size = 5;
+    int end = 4;
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
 int main()
 {
-    int arr[] = {1, 1, 1, 2, 2, 3, 4, 4, 5, 5};
-    int ans = 0;
-    for (int i; arr)
-    {
-        ans ^= i;
-    }
-    cout << ans << endl;
+    int arr[] = {1, 2, 3, 4, 5};
+    // int size = sizeof(arr) / sizeof(arr[0]);
+    // int ans = 0;
+    reverseArray(arr);
+
+    // cout << ans << endl;
 }
